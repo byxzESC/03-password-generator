69f8bbd2a8ddf1667411ae94a56b2c0fd446ac99
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2022-12-21 23:20:24 -0800

    added screenshots for readme

--- script.js
+++ script.js
@@ -22,9 +22,11 @@ function writePassword() {
     return;
   }
   // prompt to ask user for their preferences
+  console.log('user chosen ' + passwordLength + ' characters password length!');
   userPref();
   // using the user preference to combine password pool
   // iterate over user chosen length and randomize chosen character types for the password
+
   var password = generatePassword(passwordLength);
   var passwordText = document.querySelector("#password");
 
@@ -34,15 +36,19 @@ function writePassword() {
 // user preferences
 function userPref() {
   if (confirm("Would you like numeric characters include in password?")) {
+    console.log('user added numeric to the password generator.');
     poolOfCharacters += numeric;
   }
   if (confirm("Would you like lowercase characters include in password?")) {
+    console.log('user added lowercase characters to the password generator.');
     poolOfCharacters += lowerCase;
   }
   if (confirm("Would you like uppercase characters include in password?")) {
+    console.log('user added uppercase characters to the password generator.');
     poolOfCharacters += lowerCase.toUpperCase();
   }
   if (confirm("Would you like special characters include in password?")) {
+    console.log('user added special characters to the password generator.');
     poolOfCharacters += special;
   }
   
@@ -55,13 +61,13 @@ function userPref() {
 
 // function for generating a valid password for user
 function generatePassword(length) {
-  console.log(poolOfCharacters);
+  console.log('user preferences for possible characters used for password:\n' + poolOfCharacters);
   var result = "";
   // generate a password with the length user giving and preferences that user chosen 
   for (var i = 0; i < length; i++) {
     result += poolOfCharacters[Math.floor(Math.random() * poolOfCharacters.length)];
   }
-  console.log("random password is " + result);
+  console.log("random generated password is " + result);
   // resetting password preferences
   poolOfCharacters = "";
   return result;
